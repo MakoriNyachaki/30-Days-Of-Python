d1a03cef860ca549c5bda33660754192f31def13
Makori Nyachaki <nyachakim@gmail.com>
2023-05-26 15:41:31 +0300

    Function printing members of a list

---
+++ 11_Day_Functions/func_practice/list.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+
+
+def print_list(li):
+    """
+    A function that takes in a list as its parameter
+    and prints its elemenets
+    """
+    for i in li:
+        print(f"{i}")
+
+
+list1 = [23, 56, 89, 90]
+
+print_list(list1)
